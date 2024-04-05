a29d3fa2a03b4ce7118deb9d941c1dfb5d36ed25
Emika2710 <164334957+Emika2710@users.noreply.github.com>
2024-04-05 11:36:34 +0200

    ??

--- Uebungsstunde_1/src/Dozierender.java
+++ Uebungsstunde_1/src/Dozierender.java
@@ -1,42 +1,51 @@
 public class Dozierender {
-    private String name; 
+    private String name;
     private String fakultaet;
     private String bueronummer;
-    private String[][] buerozeiten = {{"Montag", "Dienstag","Mittwoch"}, {"13.00-14.00","12.30-14.00","10.00-10-30"}}; 
+    private String[][] buerozeiten = { { "Montag", "Dienstag", "Mittwoch" },
+            { "13.00-14.00", "12.30-14.00", "10.00-10-30" } };
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getFakultaet() {
         return fakultaet;
     }
+
     public void setFakultaet(String fakultaet) {
         this.fakultaet = fakultaet;
     }
+
     public String getBueronummer() {
         return bueronummer;
     }
+
     public void setBueronummer(String bueronummer) {
         this.bueronummer = bueronummer;
-    } 
+    }
+
     public String[][] getBuerozeiten() {
         return buerozeiten;
     }
+
     public void setBuerozeiten(String[][] buerozeiten) {
         this.buerozeiten = buerozeiten;
     }
-   
+
     public String answerQuestion() {
         String antwort;
-        antwort = Input.readString(); 
+        antwort = Input.readString();
         return antwort;
     }
+
     public void feedback() {
 
-        //Eingabe des Feedbacks
+        // Eingabe des Feedbacks
         System.out.println("Unterrichtsqualität bewerten von 1 bis 5");
         String antwort1 = Input.readString();
         System.out.println("generelle Stimmung bewerten von 1 bis 5");
@@ -44,9 +53,9 @@ public class Dozierender {
         System.out.println("Kompetenz des Dozierenden bewerten von 1 bis 5");
         String antwort3 = Input.readString();
 
-        //Ausgabe des Feedbacks
-        System.out.println("Unterrichtsqualität"+antwort1);
-        System.out.println("generelle Stimmung"+antwort2);
-        System.out.println("Kompetenz des Dozierenden"+antwort3);
-    } 
+        // Ausgabe des Feedbacks
+        System.out.println("Unterrichtsqualität" + antwort1);
+        System.out.println("generelle Stimmung" + antwort2);
+        System.out.println("Kompetenz des Dozierenden" + antwort3);
+    }
 }
