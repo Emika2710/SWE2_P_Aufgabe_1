f9f50c2ca5dbcc78cf632a8543c8db8465158713
Emika2710 <164334957+Emika2710@users.noreply.github.com>
2024-04-05 11:23:16 +0200

    ?

--- Uebungsstunde_1/src/Studierender.java
+++ Uebungsstunde_1/src/Studierender.java
@@ -2,7 +2,7 @@ public class Studierender {
     private String name;
     private int matrikelnummer;
     private String studiengang;
-   
+
     public String getName() {
         return name;
     }
