2b9e599e821740b547e6f484297c1f0caa53445f
Emika2710 <164334957+Emika2710@users.noreply.github.com>
2024-04-05 09:35:20 +0200

    Teil Student

---
+++ Uebungsstunde_1/src/Studierender.java
@@ -0,0 +1,34 @@
+public class Studierender {
+    private String name;
+    private int matrikelnummer;
+    private String studiengang;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMatrikelnummer() {
+        return matrikelnummer;
+    }
+
+    public void setMatrikelnummer(int matrikelnummer) {
+        this.matrikelnummer = matrikelnummer;
+    }
+
+    public String getStudiengang() {
+        return studiengang;
+    }
+
+    public void setStudiengang(String studiengang) {
+        this.studiengang = studiengang;
+    }
+
+    public void frageStellen(String frage, String professor) {
+        System.out.println("An:" + professor);
+        System.out.println(frage);
+    }
+}
