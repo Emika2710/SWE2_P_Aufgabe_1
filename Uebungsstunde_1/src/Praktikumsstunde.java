fb019176775b45689f7feaa238b27b07d2e25d15
Emika2710 <164334957+Emika2710@users.noreply.github.com>
2024-04-09 12:28:39 +0200

    Format

--- Uebungsstunde_1/src/Praktikumsstunde.java
+++ Uebungsstunde_1/src/Praktikumsstunde.java
@@ -5,39 +5,48 @@ public class Praktikumsstunde {
     private String raum;
     private String aufgaben;
 
-    //Getter und Setter
+    // Getter und Setter
     public String getThema() {
         return thema;
     }
+
     public void setThema(String thema) {
         this.thema = thema;
     }
+
     public String getDatum() {
         return datum;
     }
+
     public void setDatum(String datum) {
         this.datum = datum;
     }
+
     public String getUhrzeit() {
         return uhrzeit;
     }
+
     public void setUhrzeit(String uhrzeit) {
         this.uhrzeit = uhrzeit;
     }
+
     public String getRaum() {
         return raum;
     }
+
     public void setRaum(String raum) {
         this.raum = raum;
     }
+
     public String getAufgaben() {
         return aufgaben;
     }
+
     public void setAufgaben(String aufgaben) {
         this.aufgaben = aufgaben;
     }
 
-    //Methode
+    // Methode
     public void praktikumsstunde() {
         System.out.println(thema);
         System.out.println(datum);
